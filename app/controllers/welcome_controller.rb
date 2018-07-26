e911fa360bc1ba46e75caa693a30f598442c0f5e
admin <admin@example.net>
2018-07-26 17:59:11 +0100

    auto print with pdf

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -133,66 +133,29 @@ class WelcomeController < ApplicationController
     respond_to do |format|
       format.html{}
       format.pdf{
-        pdf = print_badge(@visitor)
-        send_data pdf.render, filename:'visitor.pdf', type: "application/pdf", disposition: "inline"
+        # pdf = print_badge(@visitor)
+        #
+        # # create a pdf
+        # # pdf = render_to_string pdf: "some_file_name", template: "templates/pdf", encoding: "UTF-8"
+        #
+        # # # then save to a file
+        # # # save_path = Rails.root.join('pdfs','filename.pdf')
+        pdf = Prawn::Document.new
+        pdf.text "Help! I am trapped in a PDF factory!"
+        pdf.print
+        File.open(File.join(Rails.root, 'public', 'pdfs', "filename.pdf"), 'wb') { |f| f.write pdf }
+        # # system("lpr", "#{Rails.root}/public/pdfs/filename.pdf")
+        # send_data pdf.render, filename:'visitor.pdf', type: "application/pdf", disposition: "inline"
+        # pdf.print
+        #
+        # ff = "/tmp/prawn_autoprint.pdf"
+        #
+        # pdf = Prawn::Document.new
+        # pdf.text "Help! I am trapped in a PDF factory!"
+        # pdf.print
+        # pdf.render_file ff
       }
     end
 
-  end
-
-  private
-
-  def print_badge(visitor)
-    Prawn::Document.new(:page_layout => :landscape) do |pdf|
-      data = StringIO.new( Base64.decode64(visitor.avatar.sub('data:image/jpeg;base64', '') ))
-
-      logo_table= pdf.make_table([  [
-                                        { :image=> data, image_width: 290, image_height: 350  }
-                                    ]],
-                                 :column_widths => [290],  :cell_style=> {
-              width: 300,
-              font_size: 20,
-              align: :center,
-              padding: [40,5,10,5],
-              borders: []
-
-          })
-
-      pdf.table([["<b>MicroHealth, LLC</b>"]], :cell_style=> { size: 60 , :inline_format=> true, align: :center,    width: 720, borders: [:bottom] })
-
-
-
-      date_creation_table= pdf.make_table([
-                                           ["#{visitor.name}"],
-                                           ["Destination:"],
-                                           ["#{visitor.last_visit.person.try(:name)}"],
-                                           ["#{visitor.last_visit.sign_in_date.strftime('%d, %B %Y')}"],
-                                           ["Time: #{visitor.last_visit.sign_in_date.strftime('%I:%M %p')}"],
-
-                                          ],
-                                          :column_widths => [420], :cell_style=> {
-              width: 420,
-              size: 48,
-              align: :left,
-              font_style: :italic,
-              borders: [],
-              padding: [24,0,0,30]
-          })
-
-
-
-      pdf.table([
-                    [
-                        logo_table,
-                        date_creation_table
-
-                    ]
-                ],
-                :column_widths => [300, 430],
-                :cell_style=> {
-                    align: :center,
-                    borders: []
-                })
-    end
   end
 end
