68b92ecd3b7c7b54311969a3a6bcd12ae357e9f0
bluenevus <frank.tucker@microhealthllc.com>
2019-04-17 12:10:29 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,13 @@ User.create(full_name: 'admin', email: 'admin@example.com', password: 'admin@201
 if AdminLoginPage.count == 0
   AdminLoginPage.create(
     section_1_image: '',
-    section_1_body: 'Need to share files with MicroHealth? If we are expecting a file from you, we will approve your account and you can send us a file.',
+    section_1_body: 'MicroHealth Visitor Management System (mVMS) is the recording and collection of visitor data — manually or through automation — for the purpose of knowing who is in ​​a facility, who they are visiting and how long they spent there.
+          .',
     section_1_file: '',
     section_2_image: '',
-    section_3_title: 'About MicroHealth Send?',
-    section_3_body: 'Microhealth Send is the easy way to move large files in and out of MicroHealth with encryption at rest and encryption in motion. Users can request files be sent to them securely or send a file securely. Up to 2 gigs in file size, you can have the file send at a future date. delete at a certain date or number of downloads.',
-    section_4_title: 'Not just your average managed file transfer.',
-    section_4_body: 'This isn\'t a shared system, this is a private one owned and managed by YOU. Easily configured to have your own branding. Its completely open source on github https://github.com/MicroHealthLLG/mSend so please contribute to make this better for everyone.'
+    section_3_title: 'About MicroHealth Visitor Management System?',
+    section_3_body: '  MicroHealth Visitor Management System (mVMS)  management system maximizes your front desk’s efficiency by allowing the receptionist to work on other important projects while your visitors sign themselves in with the Sign in.',
+    section_4_title: 'Made for Government Contractors',
+    section_4_body: 'Visitor management software electronically monitors and records information about visitors and employees of a business or public building. The software also documents and gathers information about a visitor’s whereabouts in the facility.'
   )
 end
