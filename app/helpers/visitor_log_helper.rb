2c53de2a48d632008584f800f95adff1dddcea24
bilel-kedidi <bilel.kedidi@gmail.com>
2019-12-27 11:55:21 +0100

    change button to span

--- app/helpers/visitor_log_helper.rb
+++ app/helpers/visitor_log_helper.rb
@@ -1,16 +1,16 @@
 module VisitorLogHelper
   def show_sign_in_status(visit)
     if visit.status[0] == "0"
-      '<button class="new_user_btn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></button>'.html_safe
+      '<span class="new_user_btn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>'.html_safe
     else
-      '<button class="return_user_btn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></button>'.html_safe
+      '<span class="return_user_btn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>'.html_safe
     end
   end
  def show_visit_status(visit)
    case visit.status[1..-1]
-   when  VisitorVisitInformation::SIGN_IN_OUT_RECORDED  then '<button class="visitor_signout_recored_btn"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></button>'.html_safe
-   when  VisitorVisitInformation::MUST_SIGN_OUT  then '<button class="missed_signout_btn"><span class="glyphicon glyphicon-alert" aria-hidden="true"></span></button>'.html_safe
-   when  VisitorVisitInformation::ADMIN_SIGN_IN_OUT_RECORDED  then '<button class="missed2_signout_btn"><span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true"></span></button>'.html_safe
+   when  VisitorVisitInformation::SIGN_IN_OUT_RECORDED  then '<span class="visitor_signout_recored_btn"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></span>'.html_safe
+   when  VisitorVisitInformation::MUST_SIGN_OUT  then '<span class="missed_signout_btn"><span class="glyphicon glyphicon-alert" aria-hidden="true"></span></span>'.html_safe
+   when  VisitorVisitInformation::ADMIN_SIGN_IN_OUT_RECORDED  then '<span class="missed2_signout_btn"><span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true"></span></span>'.html_safe
    else
      'hello'
    end
